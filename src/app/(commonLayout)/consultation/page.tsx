2df4f3582d0e9bd1f399d4ae70d0a320bd75c5fd
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-11-05 18:57:23 +0600

    routing-setup-in-the-project

---
+++ src/app/(commonLayout)/consultation/page.tsx
@@ -0,0 +1,7 @@
+export default function ConsultaTionPage() {
+    return (
+        <div>
+            <h1>This is ConsultaTionPage</h1>
+        </div>
+    );
+}
---
+++ src/app/(dashboardLayout)/(patientDashboardLayout)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function PatientDashboardPage() {
+    return (
+        <div>
+            <h1>This is PatientDashboardPage</h1>
+        </div>
+    );
+}
