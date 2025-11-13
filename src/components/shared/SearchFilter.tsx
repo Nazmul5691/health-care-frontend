aa87b326a8831263f4ef26465e3f70436df2fe17
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-11-13 23:07:40 +0600

    add search filter

--- src/components/shared/SearchFilter.tsx
+++ src/components/shared/SearchFilter.tsx
@@ -42,6 +42,7 @@ const SearchFilter = ({
         });
     }, [debouncedValue, paramName, router, searchParams]);
 
+    
     return (
         <div className="relative">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
