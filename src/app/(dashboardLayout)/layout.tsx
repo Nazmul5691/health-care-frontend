08fd90f945672b2bcd686018bce08fd10feea487
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-11-16 21:06:21 +0600

    delete branch-2

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -12,7 +12,7 @@ const CommonDashboardLayout = async ({ children }: { children: React.ReactNode }
                 <DashboardNavbar />
                 <main className="flex-1 overflow-y-auto bg-muted/10 p-4 md:p-6">
 
-                    <div className="max-w-7xl">{children}</div>
+                    <div className="">{children}</div>
 
                 </main>
             </div>
